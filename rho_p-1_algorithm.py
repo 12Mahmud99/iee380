f13ca3a0832e95d243445055163b5c3f7024362c
Mahmoud12 <106562348+12Mahmud99@users.noreply.github.com>
2025-04-23 22:32:29 -0700

    Update rho_p-1_algorithm.py

--- rho_p-1_algorithm.py
+++ rho_p-1_algorithm.py
@@ -1,81 +1,34 @@
-# Python code for Pollard p-1 
-# factorization Method
-	
-# importing "math" for 
-# calculating GCD
 import math
-	
-# importing "sympy" for 
-# checking prime
 import sympy
-	
-	
-# function to generate 
-# prime factors
+
 def pollard(n):
-	
-	# defining base
 	a = 2
-	
-	# defining exponent
 	i = 2
 	
-	# iterate till a prime factor is obtained
 	while(True):
-	
-		# recomputing a as required
 		a = (a**i) % n
-	
-		# finding gcd of a-1 and n
-		# using math function
-		d = math.gcd((a-1), n)
-	
-		# check if factor obtained
+		d = math.gcd((a-1), n)	
 		if (d > 1):
-	
-			#return the factor
 			return d
-	
 			break
-	
-		# else increase exponent by one 
-		# for next round
 		i += 1
 
-# Driver code
-n = 21
+n = 21 #exmaple 
 	
-# temporarily storing n
 num = n
-	
-# list for storing prime factors
 ans = []
 	
-# iterated till all prime factors
-# are obtained
-while(True):
-	
-	# function call
-	d = pollard(num)
-	
-	# add obtained factor to list
+while(True):	
+	d = pollard(num)	
 	ans.append(d)
 	
-	# reduce n
 	r = int(num/d)
 	
-	# check for prime using sympy
-	if(sympy.isprime(r)):
-	
-		# both prime factors obtained
+	if(sympy.isprime(r)):	
 		ans.append(r)
-	
 		break
 	
-	# reduced n is not prime, so repeat
 	else:
-	
 		num = r
 
-# print the result
 print("Prime factors of", n, "are", *ans)
